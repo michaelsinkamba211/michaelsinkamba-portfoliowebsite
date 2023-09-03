135e01a21d675f01ca7581a5c530559d5274fbbe
Michael Sinkamba <66191729+michaelsinkamba211@users.noreply.github.com>
2023-09-03 23:52:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,73 +2,73 @@
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
-        const sections = document.querySelectorAll("section");
-        const links = document.querySelectorAll(".main-ul li a");
+// document.addEventListener("DOMContentLoaded", function() {
+//         const sections = document.querySelectorAll("section");
+//         const links = document.querySelectorAll(".main-ul li a");
     
-        function setActiveLink(index) {
-            links.forEach(link => link.classList.remove("active"));
-            links[index].classList.add("active");
-        }
+//         function setActiveLink(index) {
+//             links.forEach(link => link.classList.remove("active"));
+//             links[index].classList.add("active");
+//         }
     
-        function getCurrentSection() {
-            for (let i = sections.length - 1; i >= 0; i--) {
-                if (sections[i].getBoundingClientRect().top <= window.innerHeight / 2) {
-                    return i;
-                }
-            }
-            return 0;
-        }
+//         function getCurrentSection() {
+//             for (let i = sections.length - 1; i >= 0; i--) {
+//                 if (sections[i].getBoundingClientRect().top <= window.innerHeight / 2) {
+//                     return i;
+//                 }
+//             }
+//             return 0;
+//         }
     
-        window.addEventListener("scroll", function() {
-            const currentSectionIndex = getCurrentSection();
-            setActiveLink(currentSectionIndex);
-        });
-    });
-    
-    
+//         window.addEventListener("scroll", function() {
+//             const currentSectionIndex = getCurrentSection();
+//             setActiveLink(currentSectionIndex);
+//         });
+//     });
     
     
     
     
     
-    document.addEventListener("DOMContentLoaded", function() {
-        const sections = document.querySelectorAll("section");
-        const links = document.querySelectorAll(".main-ul li a");
     
-        function setActiveLink(index) {
-            links.forEach(link => link.classList.remove("active"));
-            links[index].classList.add("active");
-        }
     
-        function getCurrentSection() {
-            for (let i = sections.length - 1; i >= 0; i--) {
-                if (sections[i].getBoundingClientRect().top <= window.innerHeight / 2) {
-                    return i;
-                }
-            }
-            return 0;
-        }
+//     document.addEventListener("DOMContentLoaded", function() {
+//         const sections = document.querySelectorAll("section");
+//         const links = document.querySelectorAll(".main-ul li a");
     
-        window.addEventListener("scroll", function() {
-            const currentSectionIndex = getCurrentSection();
-            setActiveLink(currentSectionIndex);
-        });
-    });
+//         function setActiveLink(index) {
+//             links.forEach(link => link.classList.remove("active"));
+//             links[index].classList.add("active");
+//         }
     
-
-
-    // for mobile
-
+//         function getCurrentSection() {
+//             for (let i = sections.length - 1; i >= 0; i--) {
+//                 if (sections[i].getBoundingClientRect().top <= window.innerHeight / 2) {
+//                     return i;
+//                 }
+//             }
+//             return 0;
+//         }
+    
+//         window.addEventListener("scroll", function() {
+//             const currentSectionIndex = getCurrentSection();
+//             setActiveLink(currentSectionIndex);
+//         });
+//     });
     
 
 
-// email JS
+//     // for mobile
 
-// https://youtu.be/5EZsRnJpUNU/
+    
 
 
-// get all needed elements from the DOM
+// // email JS
+
+// // https://youtu.be/5EZsRnJpUNU/
+
+
+// // get all needed elements from the DOM
 
 
 
